d90618e60c9dbd84ab888a5a720eb4a692a275b1
lobash <zhenyacitx@yandex.ru>
2016-01-12 16:12:03 +0300

    fix 2button + comment some code

--- src/Process/Game.java
+++ src/Process/Game.java
@@ -12,9 +12,11 @@ public class Game extends JFrame {
     public JTextField input = new JTextField();
 
     public static JTextArea textArea = new JTextArea(10, 30);
-    public static JButton button = new JButton("Start Game");
+
     public static String answer;
     public static String message;
+
+    JButton button = new JButton("Start Game");
     JButton button2 = new JButton("Enter");
     JButton button3 = new JButton("Enter");
 
@@ -24,8 +26,11 @@ public class Game extends JFrame {
         super("Max-Javax");
         this.setBounds(700, 550, 400, 600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+//      настраиваю текстовое поле: недоступно для редактирования, и перенос слов на новую линию
         textArea.setWrapStyleWord(true);
         textArea.setEditable(false);
+        textArea.setText("Нажмите Start Game");
 
 
         Container container = this.getContentPane();
@@ -35,48 +40,49 @@ public class Game extends JFrame {
         container.add(button);
 
 
+
+
+//      привязываем слушателя к первой кнопке
+
         button.addActionListener(e -> {
+            message = "Здравствуйте! \n" + "Прежде чем начать эту умопомрачительную игру, просьба ответить на небольшой вопрос.";
+            message += "На чем вы пишете? (ввести число) "
+                    + "\n A. Java"
+                    + "\n B. PHP ;"
+                    + "\n C. тетрадь (любое другое)";
 
-            String message1 = "Здравствуйте! \n" + "Прежде чем начать эту умопомрачительную игру, просьба ответить на небольшой вопрос.";
-            message1 += "На чем вы пишете? "
-                    + "\n 1. Java"
-                    + "\n 2. PHP (ввести число);"
-                    + "\n 3. тетрадь (любое другое)";
+//          обрабатываем ответ, выводим следующую кнопку
 
-            answer = input.getText();
-            textArea.setText(message1);
-            System.out.println(answer);
+//            answer = input.getText();
+            textArea.setText(message);
+//            System.out.println(answer);
             container.remove(button);
             container.add(button2);
-            repaint();
+//            repaint();
         });
 
-//        у второго слушателя включаем свитч, ему передаем параметр (ответ)
-//        обрабатываем, и пляшем к третей кнопке или гамовер. Осталось только вывести это
-
-//        Выводятся три кнопки сразу, пофиксить
+    /* у второго слушателя включаем сверку на ответ, обрабатываем,
+       и пляшем к третей кнопке или гамовер с ресетом. */
 
         button2.addActionListener(e -> {
-            switch (answer) {
-                case "1":
-                    message = "Вот и наш ГГ Максим хотел писать на Java, \n однако работа заставляла писать его на PHP. Давайте вместе поможем Максу написать проект на Java? Тогда вперед!";
-                    textArea.setText(message);
-                    container.add(button3);
+            answer = input.getText();
+            System.out.println(answer); //проверка введенного значения
 
 
-
-                case "2":
-                    message = "GAME OVER(без обоснований)";
-                    textArea.setText(message);
-                    container.add(button);
-
-
-                case "3":
-                    message = "GAME OVER(без обоснований)";
-                    textArea.setText(message);
-                    container.add(button);
-
+            if (answer.equals ("A")) {
+                message = "Вот и наш ГГ Максим хотел писать на Java, \n однако работа заставляла писать его на PHP. Давайте вместе поможем Максу написать проект на Java? Тогда вперед!";
+                textArea.setText(message);
+                container.add(button3);
+            } else {
+                message = "GAME OVER(без обоснований222)";
+                textArea.setText(message);
+                container.add(button);
             }
+
+        });
+
+        button3.addActionListener(e -> {
+
         });
     }
 
