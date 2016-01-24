4009dedda6f5dc0aa8319552c8384126ff023d35
lobash <zhenyacitx@yandex.ru>
2016-01-24 23:03:43 +0300

    reliase

--- src/Process/Listener.java
+++ src/Process/Listener.java
@@ -1,9 +1,6 @@
 package Process;
 
 
-import javax.swing.*;
-import java.math.*;
-
 public class Listener extends Game {
 
     // Включаем слушатели, реализация основного игрового процесса.
@@ -11,24 +8,34 @@ public class Listener extends Game {
     public Listener() {
 
 
-        //слушатель старт гейма
+        //слушатель старт гейма button
 
         button.addActionListener(e -> {
             button.setVisible(false);
             remove(button);
             repaint();
-            textArea.setText("Вступительный текст!");
+            textArea.setText("Вы - программист,работающий на PHP. " +
+                    "Однако, Вы не отчаиваетесь, и желаете изучать Java, но Ваше начальство разумеется против. " +
+                    "Итак, Ваша основная цель - привести проект к 100% готовности." +
+                    "Вперед!!!");
+            stockElement();
 
+
+//      Добавляем кнопку button5 для php
             button5.setVisible(true);
             button5.setText("Work on PHP");
+//      Добавляем кнопку button2 для Java
 
             button2.setVisible(true);
             button2.setText("Work on Java");
-            button2.addActionListener(e1 -> {
-            });
+
+            //      Добавляем кнопку button3 для плагиата
+
 
             button3.setVisible(true);
             button3.setText("Cтырить код из инета");
+
+            //      Добавляем кнопку button4 для мольбы
 
             button4.setVisible(true);
             button4.setText("Мольба макаронному богу");
@@ -36,37 +43,45 @@ public class Listener extends Game {
         });
 
 
-        //слушатель кнопки 1
+        //слушатель кнопки b5 для php
         button5.addActionListener(e1 -> {
             textArea.setText("Начальник видит, что Вы ответственно выполняете свои обязанности. " +
                     "Его бдительность уменьшена ");
             step += 1;
-            procent -= 10.0;
-            System.out.print(procent + " ");
-            System.out.print(step + " ");
+            procent -= 1.0;
+            newDay();
+            totalGame();
+            System.out.println("step = "+step);
+            System.out.println("day = " + day);
+            System.out.println(" ");
         });
 
-//  слушатель кнопки 2
+//  слушатель кнопки b2 для Java
         button2.addActionListener(e2 -> {
             if (procent <= Math.random() * 10) {
-                textArea.setText(popitkaPhp +
-                        "\n и к счастью, начальник не увидел этого. Так держать!");
+                textArea.setText(popitkaJava +
+                        "\n и, к счастью, начальник не увидел этого. Так держать!");
                 step += 1;
                 proekt += Math.random() * 10;
                 proektField.setText(String.valueOf(proekt));
-//                System.out.println("step = " + step);
-//                System.out.println("proekt = " + proekt);
+
             } else {
-                textArea.setText(popitkaPhp + "\n но начальник увидел это. Он явно недоволен.");
+                textArea.setText(popitkaJava + "\n но начальник увидел это. Он явно недоволен.");
                 step += 1;
                 shtraf += 1;
                 spalilsyaField.setText(String.valueOf(shtraf));
-//                System.out.print(procent + " ");
-//                System.out.print(step + " ");
             }
+
+//            счетчики
+            newDay();
+            shtrafGameOver();
+            totalGame();
+            System.out.println("step = "+ step);
+            System.out.println("day = " + day);
+            System.out.println(" ");
         });
 
-//        Слушатель кнопки 3
+//        Слушатель кнопки b3 для плагиата
         button3.addActionListener(e -> {
             if (procent <= Math.random() * 10) {
                 textArea.setText(popitkaPlagiat + "\n Ещё и начальник заметил, отругал. " +
@@ -83,16 +98,40 @@ public class Listener extends Game {
                 proekt += Math.abs(Math.random() * 15);
                 proektField.setText(String.valueOf(proekt));
             }
+
+            newDay();
+            totalGame();
+            shtrafGameOver();
+            karmaGameOver();
+            System.out.println("step = "+step);
+            System.out.println("day = " + day);
+            System.out.println(" ");
         });
 
-//        слушатель на 4 кнопку
+//        слушатель на b4 кнопку для мольбы
+
         button4.addActionListener(e -> {
-            textArea.setText("Вы решили что надо снять с себя негатив, и молча произнесли пастафарианскую мольбу. " +
+            textArea.setText("Вы решили, что надо снять с себя негатив, и молча произнесли пастафарианскую мольбу. " +
                     "\n Сразу полегчало");
             karma = 0;
+            step += 1;
             karmaField.setText(String.valueOf(karma));
+
+            newDay();
+            totalGame();
+
+            System.out.println("step = "+step);
+            System.out.println("day = " + day);
+            System.out.println(" ");
         });
+
     }
 
+    public static void main(String[] args) {
 
+        Listener li = new Listener();
+        li.setVisible(true);
+
+
+    }
 }
--- src/Process/Spisok.java
+++ src/Process/Spisok.java
@@ -16,13 +16,78 @@ public class Spisok extends JFrame {
     public static double procent = 4.0;
 
 
-
     // Вывод текста на экран
     public static String message;
     public static String resultat;
-    final static String popitkaPhp = "Вы попытались тайком хоть что-то сделать в Вашем подпольном проекте ";
+    final static String popitkaJava = "Вы попытались тайком хоть что-то сделать в Вашем подпольном проекте ";
     final static String popitkaPlagiat = "'Ну и зачем тебе это?' - спросили вы себя. \n" +
             "'Ты ведь обещал всё писать сам!' - и стало совестно очень.";
+    public void removeButton(){
+        button.setVisible(true);
+        button2.setVisible(false);
+        button3.setVisible(false);
+        button4.setVisible(false);
+        button5.setVisible(false);
+    }
+
+//    Условие для нового дня
+    public void newDay() {
+
+        switch (step) {
+            case 5:
+                step = 0;
+                day += 1;
+                break;
+        }
+    }
+    public void shtrafGameOver (){
+        if(shtraf>=5){
+            resultat=" Начальник пронюхал твои делишки, и уволил тебя. Game Over ";
+            removeButton();
+            textArea.setText(resultat);
+        }
+    }
+
+    //          Условие для гейм овера по карме
+    public void karmaGameOver() {
+        if (karma >= 5) {
+            resultat = "Вы почувствовали знак свыше, что необходимо сходить покурить, даже если Вы не курите. /n" +
+                    "Стрельнув сигарету, отходите, и в Вас прилетает метиорит в форме макарон. Game Over";
+            removeButton();
+        }
+    }
+
+//        Условие для гейм овера по окончанию дней
+
+    public void totalGame(){
+        if(proekt>=100&&day>=5){
+            resultat="Вы выйграли! Молодец! У Вас хорошее будущее! ";
+            removeButton();
+            textArea.setText(message + resultat);
+
+            textArea.setText(message);
+        }else if(proekt<100&&day>=5){
+            resultat="Простите, но проект не закончен. Начальник узнал что вы пишете на Java и выгнал Вас, а без портфолио Вы не устроитесь на работу \n"+
+                    "Да и жена Вас скорее всего бросит. Конец. Game Over";
+            removeButton();
+            textArea.setText(message + resultat);
+        }
+        message="Ну чтож, всё хорошое в этом мире когда-либо заканчивается, и наша игра не исключение.\n"+
+                resultat;
+    }
+
+    public void stockElement(){
+        shtraf = 0;
+        spalilsyaField.setText(String.valueOf(shtraf));
+        karma = 0;
+        karmaField.setText(String.valueOf(karma));
+        proekt = 0;
+        proektField.setText(String.valueOf(proekt));
+        procent = 5.0;
+        step = 0;
+
+    }
+
 
 
     //  Элементы фрейма
